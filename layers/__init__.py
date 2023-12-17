100152e2cf900391882b229d515b73b4185d7a11
Xu Qian <1464238196@qq.com>
2023-12-17 23:18:05 +0800

    add drop_path module

--- layers/__init__.py
+++ layers/__init__.py
@@ -4,3 +4,4 @@ from .retention import RetentionBlock, SimpleRetention, MultiScaleRetention
 from .convformer import MetaFormerStage,MetaPolypConvFormerBlock
 from .duck import DuckBlock,DuckResidualBlock
 from .dsconv import DSConv3d,DSConv3dBlock
+from .drop import DropPath
